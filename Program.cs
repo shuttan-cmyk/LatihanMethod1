bd65f0a0583f8c1d011031b152cde6f0a37179e8
shuttan-cmyk <shuttanpramesti@gmail.com>
2026-01-14 14:46:02 +0700

    kode

--- Program.cs
+++ Program.cs
@@ -3,5 +3,25 @@
 void Tambah()
 {
 	c = a + b;
-	Console.WriteLine("Hasil Tambah: " + c);
+	Console.WriteLine($"{a} + {b} = {c}");
 }
+void Kurang()
+{
+	c = a + b;
+	Console.WriteLine($"{a} - {b} = {c}");
+}
+void kali()
+{
+	c = a + b;
+	Console.WriteLine($"{a} * {b} = {c}");
+}
+void bagi()
+{
+	c = a + b;
+	Console.WriteLine($"{a} / {b} = {c}");
+}
+
+Tambah();
+Kurang();
+kali();
+bagi();
